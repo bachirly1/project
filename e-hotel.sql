75ee157fb5af6569c92408fc5f9a1d28eacfd6bb
bachirly1 <91099813+bachirly1@users.noreply.github.com>
2023-03-22 02:07:17 -0400

    Add files via upload

---
+++ e-hotel.sql
@@ -0,0 +1,15 @@
+-- Database: E-hotels
+
+-- DROP DATABASE IF EXISTS "E-hotels";
+
+CREATE DATABASE "E-hotels"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+	--Creation de la table 
